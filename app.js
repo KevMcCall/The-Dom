93389b663f89195104cb4a94484855c061d6b891
kevmccall <kev_mccall@yahoo.com>
2021-01-19 19:46:16 -0700

    added password pattern and placeholders

---
+++ app.js
@@ -0,0 +1,28 @@
+
+// <!-- Add a script tag to the bottom of the HTML body.
+// // (In the JavaScript) Change the body tag's style so it has a font-family of "Arial, sans-serif".
+// document.body.style.fontFamily = "Arial,sans-serif";
+
+// // (In the JavaScript) Replace each of the spans (nickname, favorites, hometown) with your own information.
+// document.getElementById('nickname').textContent = 'Kev';
+// document.getElementById('favorites').textContent = 'food, games, technology, movies';
+// document.getElementById('hometown').textContent = 'Denver';
+
+// // Iterate through each li and change the class to "list-item".
+// var list = document.getElementsByTagName('li');
+// for (var i = 0; i < list.length; i++) {
+//     list[i].className = 'list-item';
+//  }
+ 
+//  // (In the HTML head) Add a style tag that sets a rule for .list-item to make the color red.
+ 
+ 
+//  // Create a new img element and set its src attribute to a picture of you. Append that element to the page. -->
+//  var myPicture= document.createElement('img');
+//       myPic.src = 'images/70085443.jpg';
+// // Parent node is body, child is (myPic)
+//       document.body.appendChild(myPic);
+
+
+
+      
